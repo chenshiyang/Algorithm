4228f34bea141449c9aea5cad7072a7b1d469302
chenshiyang <411030065@qq.com>
2016-09-23 13:19:12 +0800

    Add a demo of CountDownLatch

---
+++ src/csy/concurrent/CountDownLatchDemo.java
@@ -0,0 +1,36 @@
+package csy.concurrent;
+
+import java.util.Random;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * 
+ * Description: A demo of CountDownLatch.
+ */
+public class CountDownLatchDemo implements Runnable{
+	static final CountDownLatch end = new CountDownLatch(10);
+	static final CountDownLatchDemo demo = new CountDownLatchDemo();
+
+	@Override
+	public void run() {
+		try {
+			Thread.sleep(new Random().nextInt(5) * 1000);
+			System.out.println(Thread.currentThread().getId() + " check complete.");
+			end.countDown();
+		} catch(InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) throws InterruptedException {
+		ExecutorService exec = Executors.newFixedThreadPool(10);
+		for(int i = 0; i < 10; i ++) {
+			exec.submit(demo);
+		}
+		end.await();
+		System.out.println("Fire!");
+		exec.shutdown();
+	}
+}
