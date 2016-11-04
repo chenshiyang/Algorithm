40ac72701ed3c453cbee11d091b94489619f1005
csy <411030065@qq.com>
2016-11-04 10:29:23 +0800

    Create 8 queens problem.

---
+++ src/csy/backtracking/EightQueenProblem.java
@@ -0,0 +1,23 @@
+package csy.backtracking;
+
+/**
+ * 
+ * Description: 
+ */
+public class EightQueenProblem {
+	public static void backtrack() {
+		
+	}
+	
+	public static void solve() {
+		
+	}
+	
+	public static boolean dangerous() {
+		
+	}
+	
+	public static void main(String[] args) {
+		
+	}
+}
