150c5d937d75b51811e2dab1b599c24e1c78c1a5
csy <411030065@qq.com>
2016-11-08 15:30:33 +0800

    Add GenericTypesTest

---
+++ src/csy/jvm/GenericTypesTest.java
@@ -0,0 +1,17 @@
+package csy.jvm;
+
+import java.util.List;
+
+/**
+ * 
+ * Description: We can not use java's generic type to overload.
+ */
+public class GenericTypesTest {
+	public static void method(List<String> list) {
+		
+	}
+	
+//	public static void method(List<Integer> list) {
+//		
+//	}
+}
