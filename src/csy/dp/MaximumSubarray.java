0459490a771f4d5ac85379cbc26ecd4e000c62d5
chenshiyang <411030065@qq.com>
2018-06-15 16:22:08 +0800

    DP解决maxSubArray问题

--- src/csy/dp/MaximumSubarray.java
+++ src/csy/dp/MaximumSubarray.java
@@ -32,7 +32,33 @@ public class MaximumSubarray {
 		System.out.println("start is " + maxStart + " , end is " + maxEnd);
 		return nAll;
 	}
-	
+
+	/**
+	 * leetcode 58题
+	 * 给定一个数组,找到和最大的子数组,至少包含一个元素
+	 *
+	 * @param nums
+	 * @return
+	 */
+	public int maxSubArray(int[] nums) {
+		if(null == nums || nums.length < 1) {
+			return 0;
+		}
+		//记录当前最大的子数组的和
+        int maxSum = nums[0];
+		int sumEndWithCurrentElement = nums[0];
+		for(int i = 1; i < nums.length; i ++) {
+			//以nums[i]结尾的子数组的和
+			if(sumEndWithCurrentElement > 0) {//这里是以nums[i-1]结尾的子数组
+				sumEndWithCurrentElement += nums[i];//这里是以nums[i]结尾的子数组
+			} else {
+			    sumEndWithCurrentElement = nums[i];
+            }
+            maxSum = Math.max(maxSum, sumEndWithCurrentElement);
+		}
+		return maxSum;
+	}
+
 	public static void main(String[] args) {
 		MaximumSubarray so = new MaximumSubarray();
 		int[] test1 = {1, -2, 3, 5, -3, 2};//8
@@ -42,5 +68,9 @@ public class MaximumSubarray {
 		System.out.println(so.solve(test1) == 8);
 		System.out.println(so.solve(test2) == 9);
 		System.out.println(so.solve(test3) == -2);
+
+		System.out.println(so.maxSubArray(test1));
+		System.out.println(so.maxSubArray(test2));
+		System.out.println(so.maxSubArray(test3));
 	}
 }
