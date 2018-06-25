fa980abb40bd2c3cde0cce6896412c5f52f1899b
chenshiyang <411030065@qq.com>
2018-06-25 23:59:33 +0800

    KMP回顾

--- src/csy/string/KMP.java
+++ src/csy/string/KMP.java
@@ -19,6 +19,9 @@ public class KMP {
 		int j = 0;//index in needle
 		for(int i = 0; i < haystack.length(); i ++) {// i is index in haystack
 			while(j != 0 && needle.charAt(j) != haystack.charAt(i)) {
+			    //当比较到haystack[i] != needle[j]
+                //这说明haystack[i-1] 是等于needle[j-1]的
+                //而next[j-1],假设其值为k 则needle[k] 是等于needle[j-1]的，直接用它来和haystak[i]比较
 				j = next[j - 1];
 			}
 			if(needle.charAt(j) == haystack.charAt(i)) {
@@ -27,18 +30,25 @@ public class KMP {
 			if(j == needle.length()) {
 				System.out.println("Pattern occurs with shift " + (i - needle.length() + 1));
 				j = next[j - 1];//if you want to get all the result, just set this so that it can continue to search.
-				//if you only want the get the first occurance, just break here.
+				//if you only want the get the first occurence, just break here.
 //				return i - needle.length() + 1;
 			}
 		}
 		return 0;
 	}
-	
+
+	/**
+	 * 创建部分匹配表
+	 *
+	 * @param needle
+	 * @return
+	 */
 	private int[] makeNext(String needle) {
 		int[] next = new int[needle.length()];//the array of the max prefix and suffix length.
-		int j = 0;//the prefitx/suffix length of needle.subString(0, i);
-		//here j has two meanning: 1. the length of the prefix
+		int j = 0;//the prefix/suffix length of needle.subString(0, i);
+		//here j has two meaning: 1. the length of the prefix
 		//						   2. the index of the jth element.
+		//next[0] = 0
 		for(int i = 1; i < needle.length(); i ++) {
 			while(j != 0 && needle.charAt(i) != needle.charAt(j)) {
 				j = next[j - 1];
